87f78e25a4ad5121d86d94c975fdfb4385651b58
afeather <afeather@users.noreply.github.com>
2016-09-18 01:43:23 -0400

    Update problem001.js

--- JS/problem001.js
+++ JS/problem001.js
@@ -4,14 +4,4 @@
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-(function() {
-
-    var multiples = limit => [...Array(limit-1)].map((_,i) => i + 1).reduce((sum, val) => val % 3 == 0 || val % 5 == 0 ? sum + val : sum, 0);
-
-    var start = new Date();
-    var out = multiples(1000);
-    var end = new Date();
-
-    console.log(out, end.getTime() - start.getTime(), 'ms');
-
-})()
+for(n=0,s=0;n<1e3;n++)s+=n%3&&n%5?0:n;
