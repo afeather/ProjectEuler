6e64bec875e4eb7a2f2c8311803e9d8226eafe03
afeather <afeather@users.noreply.github.com>
2016-09-18 02:01:27 -0400

    Update problem010.js

--- JS/problem010.js
+++ JS/problem010.js
@@ -3,25 +3,4 @@
 //
 // Find the sum of all the primes below two million.
 
-(function() {
-
-    var primesum = function(limit) {
-
-        var isPrime = [...Array(limit)].map(_ => true);
-        isPrime[0] = isPrime[1] = false;
-
-        for (var i = 2; i < limit / 2; i++)
-            for (var j = 2; i * j < limit; j++)
-                isPrime[i*j] = false;
-
-        return isPrime.map((isPrime, value) => isPrime ? value : 0).filter(val => val).reduce((sum, prime) => sum + prime);
-
-    }
-
-    var start = new Date();
-    var out = primesum(2e6);
-    var end = new Date();
-
-    console.log(out, end.getTime() - start.getTime(), 'ms');
-    
-})();
+((l)=>{for(s=[false,false].concat([...Array(l-2)].map(_=>true)),a=2,b=a*a;a*a<l;(s[a]&&b<l)?b+=a:a++&&(b=a*a))s[b]=false;return s;})(2e6).reduce((s,v,i)=>v?s+i:s);
