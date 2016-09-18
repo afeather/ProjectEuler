ab808861a1d8afcb7e8124540b71711c6a333a7b
afeather <afeather@users.noreply.github.com>
2016-09-18 01:47:44 -0400

    Update problem003.js

--- JS/problem003.js
+++ JS/problem003.js
@@ -3,29 +3,4 @@
 //
 // What is the largest prime factor of the number 600851475143 ?
 
-(function() {
-
-    var primefactors = function(num) {
-
-        var d = 2;
-
-        while (num > 1) {
-
-            while (num % d == 0) num = num / d;
-
-            d = d + 1;
-
-        }
-
-        return d - 1;
-
-    }
-
-
-    var start = new Date();
-    var out = primefactors(600851475143);
-    var end = new Date();
-
-    console.log(out, end.getTime() - start.getTime(), 'ms');
-
-})();
+for(n=600851475143,i=2;n>1;i++){if(n%i==0){while(n%i==0)n/=i;}}i-1;
